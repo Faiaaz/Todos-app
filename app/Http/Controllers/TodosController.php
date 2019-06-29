6061b54fcae72b2661febebfa6d1afdd19f098a4
Fahim Faiaz <fahim7faiaz@gmail.com>
2019-06-29 10:56:14 +0600

    view function done

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodosController extends Controller
 {
     public function index(){
-        return view('todos.index');
+
+        $libra = Todo::all();
+        return view('todos.index')->with('todos',$libra);
+    }
+
+    public function show($todoId){
+
+        $todo = Todo::find($todoId);
+
+        return view('todos.show')->with('todo',$todo);
     }
 }
